bc05486260ddbce69b9f182299c08e8bfff3f76f
Kevin Orr <kevinorr54@gmail.com>
2018-11-05 17:20:00 -0500

    Added initial Catalan computation code

---
+++ src/catalan_bignum.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <gmp.h>
+
+void calculate_catalan_part(mpq_t result, const mpz_t lower, const mpz_t upper, const mpz_t n) {
+    mpq_set_ui(result, 1, 1);
+
+    mpz_t num;
+    mpz_init(num);
+    mpq_t term, denom;
+    mpq_inits(term, denom, NULL);
+
+    mpz_t k;
+    for (mpz_init_set(k, lower); mpz_cmp(k, upper) < 0; mpz_add_ui(k, k, 1)) {
+        mpz_set(num, n);
+        mpz_add(num, num, k);
+
+        mpq_set_z(denom, n);
+
+        mpq_set_z(term, num);
+        mpq_div(term, term, denom);
+
+        mpq_canonicalize(term); // Profile this. It may be slowing down or speeding things up
+        mpq_mul(result, result, term);
+    }
+
+    mpz_clear(num);
+    mpq_clears(term, denom, NULL);
+}
---
+++ src/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <gmp.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+#define EXITERROR() error_at_line(errno, errno, __FILE__, __LINE__, "pid %llu", (long long unsigned)getpid())
+const char *argv0;
+
+void usage(FILE* f, int err) {
+    fprintf(f,
+            "USAGE: %s INDEX\n\n"
+            "Calculates the INDEXth Catalan number, using multiple threads.\n",
+            argv0);
+    exit(err);
+}
+
+int main(int argc, const char *argv[]) {
+    argv0 = argv[0];
+
+    if (argc != 2)
+        usage(stderr, 1);
+
+    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)
+        usage(stdout, 0);
+
+    mpz_t n;
+    if (mpz_init_set_str(n, argv[1], 10) != 0) {
+        fprintf(stderr, "Could not parse %s as an integer\n", argv[1]);
+        exit(1);
+    } else if (mpz_sgn(n) >= 0) {
+        fputs("INDEX must be a non-negative integer", stderr);
+        exit(1);
+    }
+
+    // Do multithreading stuff here...
+}
